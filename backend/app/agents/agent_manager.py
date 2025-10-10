3283ede671105b87ed50b71544092a7f239cbe40
snehal <snehalreddy@gmail.com>
2025-10-10 22:56:47 +0530

    feat: add expert agent tools for washing machine, AC, and refrigerator with delegation functionality

---
+++ backend/app/agents/agent_manager.py
@@ -0,0 +1,5 @@
+from langgraph.graph import StateGraph,END
+from langgraph.prebuilt import ToolNode
+from app.agents.state import AgentState
+
+from app.rag.generators import get_supervisor_model
---
+++ backend/app/tools/supervisor_tools.py
@@ -0,0 +1,45 @@
+from langchain_core.tools import tool
+from langchain_core.messages import AIMessage, HumanMessage
+
+from app.agents.sub_agents.ac_agent import ac_agent
+from app.agents.sub_agents.refrigerator_agent import refrigerator_agent
+from app.agents.sub_agents.washing_machine_agent import washing_machine_agent
+
+@tool
+def washing_machine_expert_agent(question: str) -> str:
+    """
+    Use this tool when you need to answer any complex questions or handle user
+    queries related to washing machines.
+    """
+    print("\nDelegating to Washing Machine Expert ---")
+    initial_state = {"messages": [HumanMessage(content=question)]}
+    final_state = washing_machine_agent.invoke(initial_state, {"recursion_limit": 5})
+    final_answer = final_state['messages'][-1].content
+
+    return final_answer
+
+@tool
+def ac_expert_agent(question: str) -> str:
+    """
+    Use this tool when you need to answer any complex questions or handle user
+    queries related to air conditioners (AC).
+    """
+    print("\nDelegating to AC Expert ---")
+    initial_state = {"messages": [HumanMessage(content=question)]}
+    final_state = ac_agent.invoke(initial_state, {"recursion_limit": 5})
+    final_answer = final_state['messages'][-1].content
+
+    return final_answer
+
+@tool
+def refrigerator_expert_agent(question: str) -> str:
+    """
+    Use this tool when you need to answer any complex questions or handle user
+    queries related to refrigerators.
+    """
+    print("\nDelegating to Refrigerator Expert ---")
+    initial_state = {"messages": [HumanMessage(content=question)]}
+    final_state = refrigerator_agent.invoke(initial_state, {"recursion_limit": 5})
+    final_answer = final_state['messages'][-1].content
+
+    return final_answer
